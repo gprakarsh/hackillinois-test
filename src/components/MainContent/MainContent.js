2295b3ebd31c2670efdb34ded0f19a96b2aa952f
gprakarsh <379@doonschool.com>
2019-09-18 22:01:06 -0500

    added race car

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MainContent.scss';
+import car from './../../assets/race_car.webp';
 
 const MainContent = () => {
     const [events, setEvents] = useState([]); //array intialization
@@ -10,14 +11,24 @@ const MainContent = () => {
         })
     }, []); //empty array to execute useEffect only once
 
-    events.sort((a,b)=>{                  //sorting fetched data based on time
-        return a.startTime-b.startTime
+    useEffect(() => {
+        window.addEventListener('scroll', (event) => {
+            console.log(document.onscroll)    
+        })
+        console.log('hit')
+    },[])
+
+
+    events.sort((a, b) => {                  //sorting fetched data based on time
+        return a.startTime - b.startTime
     })
 
     const eventsArr = events.map((event, i, arr) => {  // returning formatted HTML data
-        const tTemp = new Date(event.startTime);
-        const tTemp1 = tTemp.toLocaleTimeString().split('').slice();
-        const time = tTemp.toLocaleTimeString().split('').slice(0, 4).join('') + tTemp1.splice(7, 3).join('')
+        const tTemp = new Date(event.startTime * 1000);
+        let tTemp1 = tTemp.toLocaleTimeString().split(':').slice();
+        tTemp1 = tTemp1[tTemp1.length - 1].split(' ')[1];
+        console.log(tTemp1);
+        const time = tTemp.toLocaleTimeString().split(':').slice(0, 2).join(':') + ' ' + tTemp1;
         return (
             <div className="info" key={i}>
                 <div className="time">
@@ -34,6 +45,7 @@ const MainContent = () => {
 
     return (  //returning basic structure for MainContent
         <div className="main-content">
+            <img src={car} id="race-car" />
             <h1 id="heading">Schedule</h1>
             <div className="info-box">
                 <h2>Monday</h2>
