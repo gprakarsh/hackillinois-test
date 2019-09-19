5a5f80d63f36870261b5156805b318ed6f49e61a
gprakarsh <379@doonschool.com>
2019-09-18 20:05:51 -0500

    added comments

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,18 +3,18 @@ import axios from 'axios';
 import './MainContent.scss';
 
 const MainContent = () => {
-    const [events, setEvents] = useState([]);
+    const [events, setEvents] = useState([]); //array intialization
     useEffect(() => {
-        axios.get(`https://api.hackillinois.org/event/`).then((eTemp) => {
+        axios.get(`https://api.hackillinois.org/event/`).then((eTemp) => { //data fetching
             setEvents(eTemp.data.events)
         })
-    }, []);
+    }, []); //empty array to execute useEffect only once
 
-    events.sort((a,b)=>{
+    events.sort((a,b)=>{                  //sorting fetched data based on time
         return a.startTime-b.startTime
     })
 
-    const eventsArr = events.map((event, i, arr) => {
+    const eventsArr = events.map((event, i, arr) => {  // returning formatted HTML data
         const tTemp = new Date(event.startTime);
         const tTemp1 = tTemp.toLocaleTimeString().split('').slice();
         const time = tTemp.toLocaleTimeString().split('').slice(0, 4).join('') + tTemp1.splice(7, 3).join('')
@@ -32,7 +32,7 @@ const MainContent = () => {
     })
 
 
-    return (
+    return (  //returning basic structure for MainContent
         <div className="main-content">
             <h1 id="heading">Schedule</h1>
             <div className="info-box">
